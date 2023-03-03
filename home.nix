3fd23d39e2f991b798590a2367fa72b5050b7a17
hidrol <stefan.grambach@googlemail.com>
2023-03-03 16:39:15 +0100

    adding home.nix file

---
+++ home.nix
@@ -0,0 +1,65 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "hidrol";
+  home.homeDirectory = "/home/hidrol";
+
+	home.packages = [
+	    # pkgs is the set of all packages in the default home.nix implementation
+  ];
+	
+	   home.file.".config/nvim/init.vim".source = ~/Dotfiles/init.vim;
+
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    plugins = with pkgs.vimPlugins; [
+                	gruvbox 
+            vim-nix 
+            nerdtree 
+#            fzf-vim
+            vim-devicons
+            nvim-treesitter.withAllGrammars
+            nvim-lspconfig
+            cmp-nvim-lsp
+            cmp-buffer
+            cmp-path
+            cmp-cmdline
+            nvim-cmp
+#            lightline-vim
+            indent-blankline-nvim
+            tagbar
+            nvim-ts-rainbow
+            nvim-comment
+#            nvim-gdb
+#            vim-bufferline
+            bufferline-nvim
+            telescope-nvim
+            tokyonight-nvim
+            gruvbox-material
+
+    ];
+    # Use the Nix package search engine to find
+    # even more plugins : https://search.nixos.org/packages
+  };
+	
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
