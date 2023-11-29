4761ceccd403a63513e70bc3498076e2b1327e2c
hidrol <stefan.grambach@googlemail.com>
2023-11-29 16:05:25 +0100

    save state

--- home.nix
+++ home.nix
@@ -1,46 +1,11 @@
 { config, pkgs,lib, ... }:
 
-# let test = pkgs.writeText "test" ''
-#     hello world!
-#   '';
-# in
-
-  # let
-  # myString = "${pkgs.tmuxPlugins.resurrect}";
-  # in
-
-# let
-#   fromGitHub = rev: ref: repo: pkgs.vimUtils.buildVimPluginFrom2Nix {
-#     pname = "${lib.strings.sanitizeDerivationName repo}";
-#     version = ref;
-#     src = builtins.fetchGit {
-#       url = "https://github.com/HiPhish/nvim-ts-rainbow2";
-#       ref = ref;
-#       rev = rev;
-#     };
-#   };
-# in
-
-# let
-#
-# fromGitHub = rev: ref: repo: pkgs.vimUtils.buildVimPluginFrom2Nix {
-#     pname = "${lib.strings.sanitizeDerivationName repo}";
-#     version = ref;
-#     src = builtins.fetchGit {
-#       url = "https://github.com/HiPhish/nvim-ts-rainbow2";
-#       ref = ref;
-#       rev = rev;
-#     };
-#   };
-# in
-
 {
-  # testprograms = import ./test.nix {
-  #   inherit pkgs;
-  # };
 
   # testing modules
   imports = [ 
+    ./home/programs/no-gui.nix
+    ./home/programs/gui.nix
     ./home/test.nix 
     ./home/sway-home.nix 
   ];
@@ -52,64 +17,6 @@
   home.username = "hidrol";
   home.homeDirectory = "/home/hidrol";
 
-	home.packages = with pkgs; [
-    (pkgs.python3.withPackages (ps: [
-      ps.debugpy
-    ]))
-	  # pkgs is the set of all packages in the default home.nix implementation
-    tmux
-    neofetch
-    fzf # used for formarks
-    # glfw
-    #kitty
-    # zsh-autosuggestions
-    # zsh-syntax-highlighting
-    # zsh-vi-mode
-    #chromium
-    gnumake
-    lldb # c debugger
-    direnv # for nix shell
-    gnome.nautilus # gui finder
-    gdb # c debugger
-    ripgrep # used for telescope
-    fd # needed for telescop or recommended
-    man-pages
-    firefox
-    ccls #c language server
-    ctags
-    nil # nix language server
-    nodePackages.pyright
-    #vscode
-    screen
-    dtc # for decompiling dtb files
-    ubootTools # for extracting fitimage files
-    wireshark
-    #nmap
-    nix-index
-    autorandr
-    xclip
-    #python3
-    unzip
-    zip
-    epdfview
-    git
-    wget 
-    #tmux 
-    joplin-desktop
-    #nextcloud-client
-    #ccls #c language server
-    #ctags #for neovim
-    htop
-    usbutils
-    firefox
-    xclip
-    libusb
-    lxc
-    ntfs3g
-    pciutils
-    #wl-clipboard
-
-  ];
 	
 	home.file.".config/nvim/init.vim".source = ./init.vim;
   #home.file.".tmux.conf".source = ./tmux.conf;
---
+++ home/programs/gui.nix
@@ -0,0 +1,63 @@
+{ config, pkgs,lib, ... }:
+{
+	home.packages = with pkgs; [
+    (pkgs.python3.withPackages (ps: [
+      ps.debugpy
+    ]))
+	  # pkgs is the set of all packages in the default home.nix implementation
+    tmux
+    neofetch
+    fzf # used for formarks
+    # glfw
+    #kitty
+    # zsh-autosuggestions
+    # zsh-syntax-highlighting
+    # zsh-vi-mode
+    #chromium
+    gnumake
+    lldb # c debugger
+    direnv # for nix shell
+    gnome.nautilus # gui finder
+    gdb # c debugger
+    ripgrep # used for telescope
+    fd # needed for telescop or recommended
+    man-pages
+    firefox
+    ccls #c language server
+    ctags
+    nil # nix language server
+    nodePackages.pyright
+    #vscode
+    screen
+    dtc # for decompiling dtb files
+    ubootTools # for extracting fitimage files
+    wireshark
+    #nmap
+    nix-index
+    autorandr
+    xclip
+    #python3
+    unzip
+    zip
+    epdfview
+    git
+    wget 
+    #tmux 
+    joplin-desktop
+    #nextcloud-client
+    #ccls #c language server
+    #ctags #for neovim
+    htop
+    usbutils
+    firefox
+    xclip
+    libusb
+    lxc
+    ntfs3g
+    pciutils
+    #wl-clipboard
+
+  ];
+
+
+}
