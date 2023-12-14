7183cab741f194ad482d47a03ae3b92a8f7f257c
hidrol <stefan.grambach@googlemail.com>
2023-12-14 18:17:36 +0100

    rollback

--- home.nix
+++ home.nix
@@ -273,9 +273,8 @@
         #plugin = tmuxPlugins.resurrect;
     ];
     #richtig nerviger workaround weil .config/tmux/.conf bestimmte einstellungen ueberschreibt!!!
-    #vlt doch kein workaround sondern korrekt so
     extraConfig = '' 
-    source ./tmux.conf
+    source /home/hidrol/.config/nixpkgs/tmux.conf
     # for vim
     #set -g @resurrect-strategy-vim 'session'
     # for neovim
